852a311c1800ee9ddca8caf1dc80d59483c74ae0
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-04-15 10:24:27 +0200

    added more tests

--- test/network_utils_test.dart
+++ test/network_utils_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:platform_detail/src/network/custom_internet_address.dart';
 import 'package:platform_detail/src/network/custom_network_interface.dart';
+import 'package:platform_detail/src/network/default_network_interface_wrapper.dart';
 import 'package:platform_detail/src/network/dio_http_client.dart';
 import 'package:platform_detail/src/network/network_interface_wrapper.dart';
 import 'package:platform_detail/src/network/network_utils.dart';
@@ -17,17 +18,51 @@ class MockWrapper extends Mock implements NetworkInterfaceWrapper {}
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('PlatformDetail Tests', () {
+  group('DefaultNetworkInterfaceWrapper', () {
+
     late MockDio mockDio;
     late DioClient dioClient;
+    late NetworkInterfaceWrapper wrapper;
 
     setUpAll(() {
       mockDio = MockDio();
       dioClient = DioClient(dio: mockDio);
+      wrapper = DefaultNetworkInterfaceWrapper();
 
       PlatformDetail.forTesting(
         mockNetworkUtils: NetworkUtils(dioClient: dioClient),
       );
+    });
+
+    test('returns a non-empty list of network interfaces (may vary by system)', () async {
+      final interfaces = await wrapper.list();
+
+      // This test assumes the system has at least one non-loopback network interface.
+      expect(interfaces, isA<List<CustomNetworkInterface>>());
+
+      // Optionally assert there's at least one interface with at least one address.
+      final hasAtLeastOneIp = interfaces.any((iface) => iface.addresses.isNotEmpty);
+      expect(hasAtLeastOneIp, isTrue);
+    });
+
+    test('can filter by address type: IPv4', () async {
+      final interfaces = await wrapper.list(type: InternetAddressType.IPv4);
+
+      for (final iface in interfaces) {
+        for (final addr in iface.addresses) {
+          expect(InternetAddress.tryParse(addr.address)?.type, equals(InternetAddressType.IPv4));
+        }
+      }
+    });
+
+    test('can include loopback addresses', () async {
+      final interfaces = await wrapper.list(includeLoopback: true);
+
+      final hasLoopback = interfaces.any(
+            (iface) => iface.addresses.any((addr) => addr.isLoopback),
+      );
+
+      expect(hasLoopback, isTrue);
     });
 
     test('getPublicIp successfully returns the IP address', () async {
