fdf16840fbd67c7e4ef6d173d28f4cf168b4a6e5
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-04-15 13:15:31 +0200

    Added entitlements on macOS

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(),
       home: MyHomePage(
           title:
               'Flutter Demo Home Page ${PlatformDetail.currentGroupPlatform.name}'),
